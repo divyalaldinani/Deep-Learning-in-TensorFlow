e446f33517057d2281b97d4c51627e3d4b963d60
Divya <divyalaldinani@users.noreply.github.com>
2025-01-18 22:37:12 +0530

    adding callback function

--- extras/helper_functions.py
+++ extras/helper_functions.py
@@ -115,3 +115,12 @@ def view_random_image_and_augmented(class_names, directory, data_augmentation):
     # plt.title(f"Augme")
     plt.axis('Off')
 
+
+
+import datetime  # Ensure this is imported
+
+def create_tensorboard_callback1(dir_name, exp_name):
+    log_dir = dir_name + '/' + exp_name + '/' + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir = log_dir)
+    print(f"Saving TensorBoard log files to : {log_dir}")
+    return tensorboard_callback
